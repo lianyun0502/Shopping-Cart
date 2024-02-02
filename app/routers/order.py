22e69aee5d3333b7fac455439d342724cba892fd
ERICLI-NB\eric.li <eric.li@kaikutek.com>
2024-02-02 14:02:42 +0800

    feat: optimize order API
    - 補齊新增訂單的邏輯，新增訂單會同部更新資料庫商品的庫存，若庫存不夠將會回傳錯誤狀態碼
    - 刪除根據訂單id查詢訂單的API
    - 補齊根據user_id查詢user訂單的功能，並可以設定查詢時間

--- app/routers/order.py
+++ app/routers/order.py
@@ -1,22 +1,39 @@
 from fastapi import APIRouter, Body, Path, Query, Depends
 from fastapi.exceptions import HTTPException
 from schema import order
-from typing import List
+from typing import List, Optional
 from DataBase import table
 from DataBase.base import Session, get_db
 from sqlalchemy import select
-
+from datetime import datetime
 
 router = APIRouter(prefix="/orders", tags=["Order"])
 
 @router.post("", response_model=order.OutOrder)
-async def create_order(order: order.CreateOrder=Body(...),
-                       db: Session = Depends(get_db)):
+async def create_order(
+    user_id: str,
+    order: order.CreateOrder=Body(...),
+    db: Session = Depends(get_db)):
     '''
     Create Order API
-    透過 body 傳入訂單資訊, 並在資料庫中新增訂單
+    
+        透過 body 傳入訂單資訊, 並在資料庫中新增訂單, 並更新商品庫存
+        若商品庫存不足則回傳 400, indvestory not enough
+        若商品不存在則回傳 404 ,  Product not found
+
     '''
-    order = table.Orders(**order.model_dump())
+
+    product_query = db.query(table.Products).filter(table.Products.id == order.product_id)
+    product = product_query.scalar()
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    if product.investory < order.quantity:
+        raise HTTPException(status_code=400, detail="investory not enough")
+    
+    product_query.update({"investory": product.investory - order.quantity})
+    
+    order = table.Orders(user_id=user_id, **order.model_dump())
     db.add(order)
     db.commit()
     db.refresh(order)
@@ -29,30 +46,38 @@ async def get_all_orders(db: Session = Depends(get_db),
                          limit: int = 10):
     '''
     Get All Orders API
-    取得所有訂單資訊
+
+        取得所有訂單資訊
+        可透過 Query 參數選擇分頁
+        skip: 起始位置
+        limit: 取得筆數
     '''
     orders = db.query(table.Orders).offset(skip).limit(limit).all()
     return orders
 
 
-@router.get("/", response_model=order.OutOrder)
-async def get_order(order_id: int, db: Session = Depends(get_db)):
-    '''
-    Get Order API
-    透過 Qurey 選擇訂單，並取得訂單資訊
-    '''
-    order = db.query(table.Orders).filter(table.Orders.id == order_id).scalar()
-    if order is None:
-        raise HTTPException(status_code=404, detail="Order not found")
-
-    return order
-
-@router.get("/users/", response_model=List[order.OutOrder])
+@router.get("/{user_id}", response_model=List[order.OutOrder])
 async def get_user_orders(user_id: str, skip: int = 0,
-                         limit: int = 10, db: Session = Depends(get_db)):
+                         limit: int = 10, 
+                         start_date: Optional[datetime] = None,
+                         end_date: datetime = datetime.now(),
+                         db: Session = Depends(get_db)):
     '''
     Get User Orders API
-    透過 Qurey 選擇使用者，並取得使用者的所有訂單
+
+        透過 Path 選擇使用者，並取得使用者的訂單
+        可透過 Query 參數選擇時間區間
+        start_date: 起始時間
+        end_date: 結束時間
+        可透過 Query 參數選擇分頁
+        skip: 起始位置
+        limit: 取得筆數
     '''
-    orders = db.query(table.Orders).filter(table.Orders.user_id == user_id).offset(skip).limit(limit).all()
+    if start_date:
+        orders = db.query(table.Orders).filter(table.Orders.user_id == user_id, 
+                                               table.Orders.order_date >= start_date,
+                                               table.Orders.order_date <= end_date).offset(skip).limit(limit).all()
+    else:
+        orders = db.query(table.Orders).filter(table.Orders.user_id == user_id,
+                                               table.Orders.order_date <= end_date).offset(skip).limit(limit).all()
     return orders
--- app/schema/order.py
+++ app/schema/order.py
@@ -3,13 +3,13 @@ from time import time
 from datetime import datetime
 
 class Order(BaseModel):
-    user_id: str = Field(..., example="John Doe", max_length=50)
     product_id: int = Field(..., example=1)
     quantity: int = Field(..., example=1)
 
 
 class OutOrder(Order):
     id: int 
+    user_id: str
     order_date: datetime 
 
 
